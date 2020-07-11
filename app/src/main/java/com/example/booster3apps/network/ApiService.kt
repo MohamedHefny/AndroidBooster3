b527c6f0ffd20a47f4540bff76e7cd059b490b6a
Mohamed Hefny <hefny.7@gmail.com>
2020-07-11 12:56:30 +0200

    Edit popular movies url path.

--- app/src/main/java/com/example/booster3apps/network/ApiService.kt
+++ app/src/main/java/com/example/booster3apps/network/ApiService.kt
@@ -3,10 +3,13 @@ package com.example.booster3apps.network
 import com.example.booster3apps.models.MovieResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("/movie/popular")
-    fun getMovies(): Call<MovieResponse>
+    @GET("movie/popular")
+    fun getMovies(
+        @Query("api_key") apiKey: String
+    ): Call<MovieResponse>
 
 }
