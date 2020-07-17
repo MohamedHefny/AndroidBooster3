da6c72414c34decf0833772fbb198c877902bc7e
Mohamed Hefny <hefny.7@gmail.com>
2020-07-17 12:49:58 +0200

    Add  repository layer to the app.

--- app/src/main/java/com/example/booster3apps/MainActivity.kt
+++ app/src/main/java/com/example/booster3apps/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.booster3apps
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -7,40 +8,24 @@ import com.example.booster3apps.models.Movie
 import com.example.booster3apps.models.MovieResponse
 import com.example.booster3apps.network.ApiService
 import com.example.booster3apps.network.RetrofitClient
+import com.example.booster3apps.repositories.MoviesRepository
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
-
-    private val tag: String = MainActivity::class.java.simpleName
-
-    private lateinit var apiService: ApiService
+class MainActivity : AppCompatActivity(), MoviesRepository.MoviesCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        apiService = RetrofitClient.getClient().create(ApiService::class.java)
+        MoviesRepository.getMovies(this)
 
-        apiService.getMovies("d78d306988042530d99e166c3cb00017")
-            .enqueue(object : Callback<MovieResponse> {
-
-                override fun onResponse(
-                    call: Call<MovieResponse>, response: Response<MovieResponse>
-                ) {
-
-                    if (response.isSuccessful) {
-                        val moviesList: List<Movie> = response.body()?.results ?: listOf()
-                        main_recycler.adapter = MoviesAdapter(moviesList)
-                    }
-                }
-
-                override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-                    Log.e(tag, t.message.toString())
-                }
-
-            })
     }
+
+    override fun onMoviesReady(movies: List<Movie>) {
+        main_recycler.adapter = MoviesAdapter(movies)
+    }
+
 }
---
+++ app/src/main/java/com/example/booster3apps/repositories/MoviesRepository.kt
@@ -0,0 +1,53 @@
+package com.example.booster3apps.repositories
+
+import android.util.Log
+import com.example.booster3apps.MoviesAdapter
+import com.example.booster3apps.models.Movie
+import com.example.booster3apps.models.MovieResponse
+import com.example.booster3apps.network.ApiService
+import com.example.booster3apps.network.RetrofitClient
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+object MoviesRepository {
+
+    private val tag: String = MoviesRepository::class.java.simpleName
+
+    private val apiService = RetrofitClient.getClient().create(ApiService::class.java)
+
+    private const val apiKey = "d78d306988042530d99e166c3cb00017"
+
+    private val moviesList: MutableList<Movie> = mutableListOf()
+
+    fun getMovies(moviesCallback: MoviesCallback) {
+
+        if (moviesList.size > 0) {
+            moviesCallback.onMoviesReady(moviesList)
+            return
+        }
+
+        apiService.getMovies(apiKey).enqueue(object : Callback<MovieResponse> {
+
+            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+
+                if (response.isSuccessful) {
+                    val remoteMoviesList: List<Movie> = response.body()?.results ?: listOf()
+                    moviesList.addAll(remoteMoviesList)
+                    moviesCallback.onMoviesReady(moviesList)
+                }
+            }
+
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+                Log.e(tag, t.message.toString())
+            }
+
+        })
+    }
+
+    //**********************************************************************************************
+    interface MoviesCallback {
+        fun onMoviesReady(movies: List<Movie>)
+    }
+}
